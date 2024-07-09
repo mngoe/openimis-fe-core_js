68e6e2fe2f9ae5900b89be38a54c51cc63dd81b0
Joseph Bilongo <josephbilongo@Macbook-Air-Ynote.local>
2024-07-09 13:27:48 +0100

    fix 401 authenticate error

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,9 +4,7 @@ export function authMiddleware(store) {
   return function wrapDispatch(next) {
     return function handleAction(action) {
       if (action.type === "CORE_AUTH_ERR" && action.payload?.name === "ApiError" && action.payload.status === 401) {
-        if (action.payload?.source !== "AuthChequeDialog") {
-          return store.dispatch(authError(action.payload));
-        }
+        return store.dispatch(authError(action.payload));
       }
       return next(action);
     };
