4e841ce13fb59750239f7f6a713a793836628a34
michel-lewis <84686058+michel-lewis@users.noreply.github.com>
2025-05-08 05:47:37 -0400

    Revert "minifier core"

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,11 +3,9 @@ import { authError } from "./actions";
 export function authMiddleware(store) {
   return function wrapDispatch(next) {
     return function handleAction(action) {
-      console.log("action", action);
       if (action.type !== "CORE_AUTH_ERR" && action.payload?.name === "ApiError" && action.payload.status === 401) {
         return store.dispatch(authError(action.payload));
       }
-      console.log("next action ", action);
       return next(action);
     };
   };
