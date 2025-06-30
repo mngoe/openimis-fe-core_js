b0b3786d963f243910762fe89913e6a7d893aecf
michellewis <kouamkouammichellewis@gmail.com>
2025-06-30 08:41:59 -0400

    fix: correction on menu action size

--- src/components/generics/Searcher.js
+++ src/components/generics/Searcher.js
@@ -483,7 +483,7 @@ class Searcher extends Component {
               <ProgressOrError error={errorItems} />
             ) : (
               <Fragment>
-                <Grid container item alignItems="center" xs={8} className={classes.paperHeader}>
+                <Grid container item alignItems="center" xs={6} className={classes.paperHeader}>
                   <Grid item xs={8} className={classes.paperHeaderTitle}>
                     {!fetchingItems ? tableTitle : formatMessage(intl, "core", "table.resultsLoading")}
                   </Grid>
@@ -495,7 +495,7 @@ class Searcher extends Component {
                     />
                   </Grid>
                 </Grid>
-                <Grid container alignItems="center" item xs={4} className={classes.paperHeader}>
+                <Grid container alignItems="center" item xs={6} className={classes.paperHeader}>
                   {fetchedItems && (
                     <Grid container direction="row" justify="flex-end" className={classes.paperHeaderAction}>
                       <StyledSelectionMenu
