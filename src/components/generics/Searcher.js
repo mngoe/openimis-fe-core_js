fe2acba1995e286f7d575508f6a57b529c87fdea
vaneck237 <vaneckbilongo@gmail.com>
2026-05-22 16:54:49 +0100

    fix search with enter key

--- src/components/generics/Searcher.js
+++ src/components/generics/Searcher.js
@@ -18,7 +18,7 @@ import { withStyles, withTheme } from "@material-ui/core/styles";
 import MoreHoriz from "@material-ui/icons/MoreHoriz";
 
 import { cacheFilters, resetCacheFilters, saveCurrentPaginationPage } from "../../actions";
-import { DEFAULT } from "../../constants";
+import { DEFAULT, ENTER_KEY } from "../../constants";
 import { formatSorter, sort } from "../../helpers/api";
 import { formatMessage } from "../../helpers/i18n";
 import withModulesManager from "../../helpers/modules";
@@ -249,6 +249,7 @@ class Searcher extends Component {
   componentDidMount() {
     const cacheKey = this._getCacheKey();
     var filters = this.props.filtersCache[cacheKey] || this.props.defaultFilters || {};
+    document.addEventListener("keypress", this.handleEnter);
     this.setState(
       (state, props) => ({
         filters,
@@ -452,6 +453,14 @@ class Searcher extends Component {
     }
     return [];
   };
+
+  handleEnter = (event) => {
+    const activeName = document.activeElement.name;
+    if (event.key == ENTER_KEY && !!activeName && activeName != 'enquiryField'){
+      let filters = { ...this.state.filters };
+      this.setState({ filters }, (e) => this.applyFilters())
+    }
+  }
 
   renderSearcherActions = () => {
     const { searcherActions, classes } = this.props;
