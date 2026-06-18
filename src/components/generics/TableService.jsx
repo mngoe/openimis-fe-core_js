af2820cea7437ad20ebbab0332d05d586555bf89
vaneck237 <vaneckbilongo@gmail.com>
2026-06-18 14:59:41 +0100

    fix localSubserviceHeaders for review

--- src/components/generics/TableService.jsx
+++ src/components/generics/TableService.jsx
@@ -278,10 +278,11 @@ class Table extends Component {
 
                               <table style={{ marginTop: 10, width: "90%" }}>
                                 <tr>
-                                  <TableCell><FormattedMessage module={module} id={localSubServiceHeaders[0]} /></TableCell>
-                                  <TableCell><FormattedMessage module={module} id={localSubServiceHeaders[1]} /></TableCell>
-                                  <TableCell><FormattedMessage module={module} id={localSubServiceHeaders[2]} /></TableCell>
-                                  <TableCell><FormattedMessage module={module} id={localSubServiceHeaders[3]} /></TableCell>
+                                  {localSubServiceHeaders.map((header, index) => (
+                                    <TableCell key={index}>
+                                      <FormattedMessage module={module} id={header} />
+                                    </TableCell>
+                                  ))}
                                 </tr>
                                 {localsubServicesItemsFormattersReview &&
                                   localsubServicesItemsFormattersReview.map((s, sfidx) => {
@@ -340,10 +341,11 @@ class Table extends Component {
 
                               <table style={{ marginTop: 10, width: "90%" }}>
                                 <tr>
-                                  <TableCell><FormattedMessage module={module} id={localSubServiceHeaders[0]} /></TableCell>
-                                  <TableCell><FormattedMessage module={module} id={localSubServiceHeaders[1]} /></TableCell>
-                                  <TableCell><FormattedMessage module={module} id={localSubServiceHeaders[2]} /></TableCell>
-                                  <TableCell><FormattedMessage module={module} id={localSubServiceHeaders[3]} /></TableCell>
+                                  {localSubServiceHeaders.map((header, index) => (
+                                    <TableCell key={index}>
+                                      <FormattedMessage module={module} id={header} />
+                                    </TableCell>
+                                  ))}
                                 </tr>
                                 {localSubServicesItemsFormatters &&
                                   localSubServicesItemsFormatters.map((s, sfidx) => {
