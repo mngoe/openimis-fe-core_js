56fb4a94731d62ed362a0f015797b9ca07a0edab
vaneck237 <vaneckbilongo@gmail.com>
2026-08-10 15:19:05 +0100

    optimize yearPicker

--- src/pickers/YearPicker.js
+++ src/pickers/YearPicker.js
@@ -19,6 +19,7 @@ class YearPicker extends Component {
       max,
       required,
       withNull = true,
+      ...others
     } = this.props;
     const options = withNull
       ? [
@@ -35,7 +36,7 @@ class YearPicker extends Component {
       }))
     );
     return (
-      <SelectInput module={module} label={label} options={options} readOnly={readOnly} name={name} value={value} required={required} onChange={onChange} />
+      <SelectInput module={module} label={label} options={options} readOnly={readOnly} name={name} value={value} required={required} onChange={onChange} {...others}/>
     );
   }
 }
